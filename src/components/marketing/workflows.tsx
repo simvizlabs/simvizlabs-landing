f739f14e40dbe3188d901346b2eb0941057ffecc
Pranjal Katiyar <76843696+pranjalkatiyar@users.noreply.github.com>
2024-12-14 14:38:54 +0530

    UI fix

--- src/components/marketing/workflows.tsx
+++ src/components/marketing/workflows.tsx
@@ -1,74 +1,188 @@
-import { ArrowRightIcon } from "lucide-react";
-import Link from "next/link";
-import { BlurText } from "../ui/blur-text";
-import { Button } from "../ui/button";
-import { motion } from "framer-motion";
+
+
 import Image from "next/image";
-import Container from "../global/container";
 
-const Hero = () => {
-    return (
-      <div className="flex flex-col items-center text-center w-full max-w-5xl my-24 mx-auto z-40 relative">
-        <Container delay={0.0}>
-          <div className="pl-2 pr-1 py-1 rounded-full border border-foreground/10 hover:border-foreground/15 backdrop-blur-lg cursor-pointer flex items-center gap-2.5 select-none w-max mx-auto">
-            <div className="w-3.5 h-3.5 rounded-full bg-primary/40 flex items-center justify-center relative">
-              <div className="w-2.5 h-2.5 rounded-full bg-primary/60 flex items-center justify-center animate-ping">
-                <div className="w-2.5 h-2.5 rounded-full bg-primary/60 flex items-center justify-center animate-ping"></div>
-              </div>
-              <div className="w-1.5 h-1.5 rounded-full bg-primary flex items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+
+import  Spotlight2  from "../ui/spotlight2";
+import { SectionBadge } from "../ui/section-bade";
+
+
+
+
+export default function Workflows() {
+  return (
+    <section>
+      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+        <div className="pb-12 md:pb-20">
+          {/* Section header */}
+          <div className="mx-auto max-w-3xl pb-12 text-center md:pb-20">
+            <div className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-gradient-to-r before:from-transparent before:to-blue-200/50 after:h-px after:w-8 after:bg-gradient-to-l after:from-transparent after:to-blue-200/50">
+              <SectionBadge title="Advanced Aviation Solutions" />
             </div>
-            <span className="inline-flex items-center justify-center gap-2 animate-text-gradient animate-background-shine bg-gradient-to-r from-blue-300 via-blue-500 to-blue-700 bg-[200%_auto] bg-clip-text text-sm text-transparent">
-              Disrupting Distant Learning
-              <span className="text-xs text-secondary-foreground px-3 py-0.5 rounded-full bg-gradient-to-b from-foreground/20 to-foreground/10 flex items-center justify-center">
-                Learn More
-                <ArrowRightIcon className="w-3.5 h-3.5 ml-1 text-foreground/50" />
-              </span>
-            </span>
+            <div className="mx-auto max-w-3xl pb-12 text-center md:pb-20">
+              <h2 className="pt-10 animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.700),theme(colors.gray.700),theme(colors.gray.700),theme(colors.gray.700),theme(colors.gray.500))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
+                How Our Learning Analytics <br />
+                Enhances Aviation Training
+              </h2>
+              <p className="text-lg text-gray-700">
+                Experience aviation training redefined with predictive learning
+                & analytics. Our interactive distance learning emulator helps
+                airlines optimize training, boost efficiency, enhance safety,
+                identify knowledge gaps, and dynamically tailor programs to
+                individual needs.
+              </p>
+            </div>
           </div>
-        </Container>
-        <div className="mx-auto max-w-3xl pb-12 text-center md:pb-20">
-          <h2 className="pt-10 animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.700),theme(colors.gray.700),theme(colors.gray.700),theme(colors.gray.700),theme(colors.gray.500))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle text-4xl font-semibold text-transparent md:text-[2.75rem] md:leading-tight">
-            Next-Gen Interactive Distance Learning with Predictive Analytics
-          </h2>
-          <p className="text-lg text-gray-900">
-            Experience aviation training redefined with predictive learning &
-            analytics. Our interactive distance learning emulator helps airlines
-            optimize training, boost efficiency, enhance safety, identify
-            knowledge gaps, and dynamically tailor programs to individual needs.
-          </p>
-        </div>
-        <Container delay={0.2}>
-          <div className="flex items-center justify-center md:gap-x-6 mt-8 ">
-            <Button asChild size="lg">
-              <Link href="/app">Schedule a Call</Link>
-            </Button>
-            <Button
-              asChild
-              size="lg"
-              variant="outline"
-              className="hidden md:flex"
+          {/* Spotlight items */}
+          <Spotlight2 className="group mx-auto grid max-w-sm items-start gap-6 lg:max-w-none lg:grid-cols-3">
+            {/* Card 1 */}
+            <a
+              className="group/card relative h-full overflow-hidden rounded-2xl border-blue-700 bg-gray-200 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-gray-400/20 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-gray-500/80 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
+              href="#0"
             >
-              <Link href="#">Download our App</Link>
-            </Button>
-          </div>
-        </Container>
-        <Container delay={0.3}>
-          <div className="relative mx-auto max-w-7xl rounded-xl lg:rounded-[32px] border border-neutral-200/50 p-2 backdrop-blur-lg border-neutral-700 bg-neutral-800/50 md:p-4 mt-12">
-            <div className="absolute top-1/4 left-1/2 -z-10 gradient w-3/4 -translate-x-1/2 h-1/4 -translate-y-1/2 inset-0 blur-[10rem]"></div>
-
-            <div className="rounded-lg lg:rounded-[24px] border p-2 border-neutral-700 bg-black">
-              <Image
-                src="/images/dashboard.png"
-                alt="dashboard"
-                width={1920}
-                height={1080}
-                className="rounded-lg lg:rounded-[20px]"
-              />
-            </div>
-          </div>
-        </Container>
+              <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-100 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-300/50 after:via-gray-300/25 after:to-gray-300/50">
+                {/* Arrow */}
+                <div
+                  className="absolute right-6 top-6 flex h-8 w-8 items-center justify-center rounded-full border border-gray-700/50 bg-gray-800/65 text-gray-200 opacity-0 transition-opacity group-hover/card:opacity-100"
+                  aria-hidden="true"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={9}
+                    height={8}
+                    fill="none"
+                  >
+                    <path
+                      fill="#F4F4F5"
+                      d="m4.92 8-.787-.763 2.733-2.68H0V3.443h6.866L4.133.767 4.92 0 9 4 4.92 8Z"
+                    />
+                  </svg>
+                </div>
+                {/* Image */}
+                <Image
+                  className="inline-flex"
+                  src="/images/workflow-01.png"
+                  width={350}
+                  height={288}
+                  alt="Workflow 01"
+                />
+                {/* Content */}
+                <div className="p-6">
+                  <div className="mb-3">
+                    <span className="btn-sm relative rounded-full bg-gray-200/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
+                      <span className="bg-gradient-to-r from-blue-700 to-blue-700 bg-clip-text text-transparent">
+                        For Airlines
+                      </span>
+                    </span>
+                  </div>
+                  <p className="text-gray-700">
+                    Learning Analytic interactive emulators allow pilots to
+                    train remotely in complex, data-driven scenarios, creating
+                    performance benchmarks reviewed to refine and enhance
+                    training curriculum.
+                  </p>
+                </div>
+              </div>
+            </a>
+            {/* Card 2 */}
+            <a
+              className="group/card relative h-full overflow-hidden rounded-2xl border-blue-700 bg-gray-200 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-gray-400/20 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-gray-500/80 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
+              href="#0"
+            >
+              <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-100 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-300/50 after:via-gray-300/25 after:to-gray-300/50">
+                {/* Arrow */}
+                <div
+                  className="absolute right-6 top-6 flex h-8 w-8 items-center justify-center rounded-full border border-gray-700/50 bg-gray-800/65 text-gray-200 opacity-0 transition-opacity group-hover/card:opacity-100"
+                  aria-hidden="true"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={9}
+                    height={8}
+                    fill="none"
+                  >
+                    <path
+                      fill="#F4F4F5"
+                      d="m4.92 8-.787-.763 2.733-2.68H0V3.443h6.866L4.133.767 4.92 0 9 4 4.92 8Z"
+                    />
+                  </svg>
+                </div>
+                {/* Image */}
+                <Image
+                  className="inline-flex"
+                  src="/images/workflow-02.png"
+                  width={350}
+                  height={288}
+                  alt="Workflow 02"
+                />
+                {/* Content */}
+                <div className="p-6">
+                  <div className="mb-3">
+                    <span className="btn-sm relative rounded-full bg-gray-200/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
+                      <span className="bg-gradient-to-r from-blue-700 to-blue-700 bg-clip-text text-transparent">
+                        For Aeronautical Schools
+                      </span>
+                    </span>
+                  </div>
+                  <p className="text-gray-700">
+                    Predictive machine learning software analyzes performance
+                    data to identify and prioritize improvement areas, guiding
+                    focused and effective training programs.{" "}
+                  </p>
+                </div>
+              </div>
+            </a>
+            {/* Card 3 */}
+            <a
+              className="group/card relative h-full overflow-hidden rounded-2xl border-blue-700 bg-gray-200 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-gray-400/20 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-gray-500/80 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
+              href="#0"
+            >
+              <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-100 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-300/50 after:via-gray-300/25 after:to-gray-300/50">
+                {/* Arrow */}
+                <div
+                  className="absolute right-6 top-6 flex h-8 w-8 items-center justify-center rounded-full border border-gray-700/50 bg-gray-800/65 text-gray-200 opacity-0 transition-opacity group-hover/card:opacity-100"
+                  aria-hidden="true"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={9}
+                    height={8}
+                    fill="none"
+                  >
+                    <path
+                      fill="#F4F4F5"
+                      d="m4.92 8-.787-.763 2.733-2.68H0V3.443h6.866L4.133.767 4.92 0 9 4 4.92 8Z"
+                    />
+                  </svg>
+                </div>
+                {/* Image */}
+                <Image
+                  className="inline-flex"
+                  src="/images/workflow-03.png"
+                  width={350}
+                  height={288}
+                  alt="Workflow 03"
+                />
+                {/* Content */}
+                <div className="p-6">
+                  <div className="mb-3">
+                    <span className="btn-sm relative rounded-full bg-gray-200/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
+                      <span className="bg-gradient-to-r from-blue-700 to-blue-700 bg-clip-text text-transparent">
+                        For Nav Database Services
+                      </span>
+                    </span>
+                  </div>
+                  <p className="text-gray-700">
+                    Dynamic, scalable software deploys tailored training
+                    scenarios to user devices, using data trends and airline
+                    inputs to address critical knowledge gaps.
+                  </p>
+                </div>
+              </div>
+            </a>
+          </Spotlight2>
+        </div>
       </div>
-    );
-};
-
-export default Hero
+    </section>
+  );
+}
