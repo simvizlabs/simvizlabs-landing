c3a4ac28c57b81d8903243b255aa174907682c4e
Pranjal Katiyar <76843696+pranjalkatiyar@users.noreply.github.com>
2024-12-14 14:21:56 +0530

    UI fix

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,4 +1,4 @@
-import { Background, Companies, Connect, Container, CTA, Features, Hero, Perks, Pricing, Reviews, Wrapper } from "@/components";
+import { Background, Companies, Connect, Container, CTA, Features, Hero, Perks, Reviews, Wrapper } from "@/components";
 import { Spotlight } from "@/components/ui/spotlight";
 
 import Workflows from "@/components/marketing/workflows";
@@ -8,22 +8,16 @@ const HomePage = () => {
     return (
         <Background>
             <Wrapper className="py-20 relative">
-                <Container className="relative">
+                <Container className="relative pb-8">
                     <Spotlight
                         className="-top-40 left-0 md:left-60 md:-top-20"
                         fill="rgba(255, 255, 255, 0.5)"
                     />
                     <Hero />
                 </Container>
-                <Container className="py-8 lg:py-8">
-  <> </>
-</Container>
                 <Workflows/>
-               
                 <Features />
                 <Perks />
-               
-               
                 <Reviews />
                 <CTA />
             </Wrapper>
