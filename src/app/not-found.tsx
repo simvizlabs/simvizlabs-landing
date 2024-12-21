512f92863a622aaa52a6eb3744037f5b43c144ca
Pranjal Katiyar <76843696+pranjalkatiyar@users.noreply.github.com>
2024-12-21 16:34:19 +0530

    UI fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className="container w-full h-screen flex flex-col items-center justify-center">
       <h1 className="text-2xl font-semibold text-gray-600">Coming Soon</h1>
-      <Link className="text-blue-500 hover:text-blue-700" href="/">
+      <Link className="text-blue-500 hover:text-blue-700 cursor-pointer" href="/">
         Go Home
       </Link>
     </div>
