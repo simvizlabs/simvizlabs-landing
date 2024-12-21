4001615b7baf341325284e1c56972cc98f01832b
Pranjal Katiyar <76843696+pranjalkatiyar@users.noreply.github.com>
2024-12-21 16:37:07 +0530

    Ui fixes

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -45,41 +45,30 @@ export const SIDEBAR_LINKS: Link[] = [
 ];
 
 export const FOOTER_LINKS = [
-    {
-        title: "Product",
-        links: [
-            { name: "Home", href: "/" },
-            { name: "Features", href: "/" },
-            { name: "Pricing", href: "/" },
-            { name: "Contact", href: "/" },
-            { name: "Download", href: "/" },
-        ],
-    },
-    {
-        title: "Resources",
-        links: [
-            { name: "Blog", href: "/blog" },
-            { name: "Help Center", href: "/help-center" },
-            { name: "Community", href: "/community" },
-            { name: "Guides", href: "/guides" },
-        ],
-    },
-    {
-        title: "Legal",
-        links: [
-            { name: "Privacy", href: "/privacy" },
-            { name: "Terms", href: "/terms" },
-            { name: "Cookies", href: "/cookies" },
-        ],
-    },
-    {
-        title: "Developers",
-        links: [
-            { name: "API Docs", href: "/api-docs" },
-            { name: "SDKs", href: "/sdks" },
-            { name: "Tools", href: "/tools" },
-            { name: "Open Source", href: "/open-source" },
-            { name: "Changelog", href: "/changelog" },
-        ],
-    },
+  {
+    title: "Airlines",
+    links: [
+      { name: "Web Apps", href: "/" },
+      { name: "Distant Learning", href: "/" },
+    ],
+  },
+  {
+    title: "Schools",
+    links: [{ name: "LMS Course", href: "/" }],
+  },
+  {
+    title: "Pilots",
+    links: [
+      { name: "B747", href: "/" },
+      { name: "B737", href: "/" },
+      { name: "B767", href: "/" },
+      { name: "B757", href: "/" },
+    ],
+  },
+  {
+    title: "Developers",
+    links: [
+        { name: "API Docs", href: "/" },
+    ],
+  },
 ];
