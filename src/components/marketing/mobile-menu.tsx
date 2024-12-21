046e55cd5acf5c0bbfc46f447cb0a500ddfd02a4
Pranjal Katiyar <76843696+pranjalkatiyar@users.noreply.github.com>
2024-12-21 16:36:38 +0530

    UI fixes

--- src/components/marketing/mobile-menu.tsx
+++ src/components/marketing/mobile-menu.tsx
@@ -4,7 +4,7 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/
 import { cn } from "@/functions";
 import { useClickOutside } from "@/hooks";
 import { motion } from "framer-motion";
-import { Box, CalendarClock, Captions, CircleHelp, CopyCheck, FileText, Gem, Layers3, LineChart, Newspaper, UserCog, Waypoints } from "lucide-react";
+import { Award, Box, CalendarClock, Captions, CircleHelp, CopyCheck, FileText, Gem, Layers3, LineChart, Newspaper, Plane, University, UserCog, Waypoints } from "lucide-react";
 import Link from "next/link";
 import React from 'react';
 
@@ -23,136 +23,146 @@ const MobileMenu = ({ isOpen, setIsOpen }: Props) => {
     };
 
     return (
-        <div
-            ref={ref}
-            className={cn(
-                "absolute top-12 inset-x-0 size-full p-4 z-20 bg-inherit flex flex-1",
-                isOpen ? "flex" : "hidden"
-            )}
+      <div
+        ref={ref}
+        className={cn(
+          "absolute top-12 inset-x-0 size-full p-4 z-20 bg-inherit flex flex-1",
+          isOpen ? "flex" : "hidden"
+        )}
+      >
+        <motion.div
+          initial="closed"
+          animate={isOpen ? "open" : "closed"}
+          variants={variants}
+          transition={{
+            type: "spring",
+            bounce: 0.15,
+            duration: 0.5,
+          }}
+          className="size-full flex flex-col justify-start"
         >
-            <motion.div
-                initial="closed"
-                animate={isOpen ? "open" : "closed"}
-                variants={variants}
-                transition={{
-                    type: "spring",
-                    bounce: 0.15,
-                    duration: 0.5,
-                }}
-                className="size-full flex flex-col justify-start"
+          <ul className="flex flex-col items-start flex-1 w-full space-y-3">
+            <Accordion type="single" collapsible className="w-full">
+              <AccordionItem value="item-1" className="border-transparent">
+                <AccordionTrigger className="px-4 py-2 text-lg hover:text-muted-foreground font-normal">
+                  <span className="flex items-center">
+                    <Plane className="w-4 h-4 mr-2" />
+                    Airlines
+                  </span>
+                </AccordionTrigger>
+                <AccordionContent
+                  onClick={() => setIsOpen(false)}
+                  className="flex flex-col items-start gap-1 mt-1"
+                >
+                  <li className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80">
+                    <Link
+                      href="/"
+                      className="flex items-center w-full text-start"
+                    >
+                      <Newspaper className="w-4 h-4 mr-2" />
+                      Interactive Web Apps
+                    </Link>
+                  </li>
+                  <li className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80">
+                    <Link
+                      href="/"
+                      className="flex items-center w-full text-start"
+                    >
+                      <Newspaper className="w-4 h-4 mr-2" />
+                      Distant Learning
+                    </Link>
+                  </li>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+            <Accordion type="single" collapsible className="w-full">
+              <AccordionItem value="item-1" className="border-transparent">
+                <AccordionTrigger className="px-4 py-2 text-lg hover:text-muted-foreground font-normal">
+                  <span className="flex items-center">
+                    <University className="w-4 h-4 mr-2" />
+                    Aeronautical Schools
+                  </span>
+                </AccordionTrigger>
+                <AccordionContent
+                  onClick={() => setIsOpen(false)}
+                  className="flex flex-col items-start gap-1 mt-1"
+                >
+                  <li className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80">
+                    <Link
+                      href="/"
+                      className="flex items-center w-full text-start"
+                    >
+                      <Newspaper className="w-4 h-4 mr-2" />
+                      LMS Course
+                    </Link>
+                  </li>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+            <Accordion type="single" collapsible className="w-full">
+              <AccordionItem value="item-1" className="border-transparent">
+                <AccordionTrigger className="px-4 py-2 text-lg hover:text-muted-foreground font-normal">
+                  <span className="flex items-center">
+                    <Award className="w-4 h-4 mr-2" />
+                    Students & Professional Pilots
+                  </span>
+                </AccordionTrigger>
+                <AccordionContent
+                  onClick={() => setIsOpen(false)}
+                  className="flex flex-col items-start gap-1 mt-1"
+                >
+                  <li className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80">
+                    <Link
+                      href="/"
+                      className="flex items-center w-full text-start"
+                    >
+                      <Newspaper className="w-4 h-4 mr-2" />
+                      B747
+                    </Link>
+                  </li>{" "}
+                  <li className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80">
+                    <Link
+                      href="/"
+                      className="flex items-center w-full text-start"
+                    >
+                      <Newspaper className="w-4 h-4 mr-2" />
+                      B737
+                    </Link>
+                  </li>
+                  <li className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80">
+                    <Link
+                      href="/"
+                      className="flex items-center w-full text-start"
+                    >
+                      <Newspaper className="w-4 h-4 mr-2" />
+                      B767
+                    </Link>
+                  </li>
+                  <li className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80">
+                    <Link
+                      href="/"
+                      className="flex items-center w-full text-start"
+                    >
+                      <Newspaper className="w-4 h-4 mr-2" />
+                      B757
+                    </Link>
+                  </li>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+            <li
+              onClick={() => setIsOpen(false)}
+              className="w-full px-4 py-2 text-lg hover:text-muted-foreground font-normal transition transform rounded-md cursor-pointer text-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80"
             >
-                <ul className="flex flex-col items-start flex-1 w-full space-y-3">
-                    <li
-                        onClick={() => setIsOpen(false)}
-                        className="w-full px-4 py-2 text-lg hover:text-muted-foreground font-normal transition transform rounded-md cursor-pointer text-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80"
-                    >
-                        <Link href="/how-it-works" className="flex items-center w-full text-start">
-                            <UserCog className="w-4 h-4 mr-2" />
-                            How it works
-                        </Link>
-                    </li>
-                    <Accordion type="single" collapsible className="w-full">
-                        <AccordionItem value="item-1" className="border-transparent">
-                            <AccordionTrigger className="px-4 py-2 text-lg hover:text-muted-foreground font-normal">
-                                <span className="flex items-center">
-                                    <CopyCheck className="w-4 h-4 mr-2" />
-                                    Features
-                                </span>
-                            </AccordionTrigger>
-                            <AccordionContent onClick={() => setIsOpen(false)} className="flex flex-col items-start gap-1 mt-1">
-                                <li
-                                    className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80"
-                                >
-                                    <Link href="/" className="flex items-center w-full text-start">
-                                        <Captions className="w-4 h-4 mr-2" />
-                                        Caption Generation
-                                    </Link>
-                                </li>
-                                <li
-                                    className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80"
-                                >
-                                    <Link href="/" className="flex items-center w-full text-start">
-                                        <CalendarClock className="w-4 h-4 mr-2" />
-                                        Post Scheduling
-                                    </Link>
-                                </li>
-                                <li
-                                    className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80"
-                                >
-                                    <Link href="/" className="flex items-center w-full text-start">
-                                        <LineChart className="w-4 h-4 mr-2" />
-                                        Analytics Dashboard
-                                    </Link>
-                                </li>
-                            </AccordionContent>
-                        </AccordionItem>
-                    </Accordion>
-                    <li
-                        onClick={() => setIsOpen(false)}
-                        className="w-full px-4 py-2 text-lg hover:text-muted-foreground font-normal transition transform rounded-md cursor-pointer text-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80"
-                    >
-                        <Link href="/" className="flex items-center w-full text-start">
-                            <Gem className="w-4 h-4 mr-2" />
-                            Pricing
-                        </Link>
-                    </li>
-                    <li
-                        onClick={() => setIsOpen(false)}
-                        className="w-full px-4 py-2 text-lg hover:text-muted-foreground font-normal transition transform rounded-md cursor-pointer text-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80"
-                    >
-                        <Link href="/" className="flex items-center w-full text-start">
-                            <Waypoints className="w-4 h-4 mr-2" />
-                            Integrations
-                        </Link>
-                    </li>
-                    <Accordion type="single" collapsible className="w-full">
-                        <AccordionItem value="item-1" className="border-transparent">
-                            <AccordionTrigger className="px-4 py-2 text-lg hover:text-muted-foreground font-normal">
-                                <span className="flex items-center">
-                                    <Layers3 className="w-4 h-4 mr-2" />
-                                    Resources
-                                </span>
-                            </AccordionTrigger>
-                            <AccordionContent onClick={() => setIsOpen(false)} className="flex flex-col items-start gap-1 mt-1">
-                                <li
-                                    className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80"
-                                >
-                                    <Link href="/" className="flex items-center w-full text-start">
-                                        <Newspaper className="w-4 h-4 mr-2" />
-                                        Blog
-                                    </Link>
-                                </li>
-                                <li
-                                    className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80"
-                                >
-                                    <Link href="/" className="flex items-center w-full text-start">
-                                        <FileText className="w-4 h-4 mr-2" />
-                                        Case Studies
-                                    </Link>
-                                </li>
-                                <li
-                                    className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80"
-                                >
-                                    <Link href="/" className="flex items-center w-full text-start">
-                                        <Box className="w-4 h-4 mr-2" />
-                                        Tools
-                                    </Link>
-                                </li>
-                                <li
-                                    className="w-full px-4 py-2 text-lg font-normal transition transform rounded-md cursor-pointer text-foreground/80 hover:text-muted-foreground text-start active:scale-95 hover:bg-muted/20 active:opacity-80"
-                                >
-                                    <Link href="/" className="flex items-center w-full text-start">
-                                        <CircleHelp className="w-4 h-4 mr-2" />
-                                        Support
-                                    </Link>
-                                </li>
-                            </AccordionContent>
-                        </AccordionItem>
-                    </Accordion>
-                </ul>
-            </motion.div>
-        </div>
-    )
+              <Link href="/" className="flex items-center w-full text-start">
+                <Waypoints className="w-4 h-4 mr-2" />
+                API for Developers
+              </Link>
+            </li>
+          </ul>
+        </motion.div>
+      </div>
+    );
 };
 
 export default MobileMenu
