256604c904264ce11df912cb7c0d2854cf2cfc73
pranjal <pranjalkatiyar>
2025-01-04 15:02:48 +0530

    ui updates

--- src/components/marketing/workflows.tsx
+++ src/components/marketing/workflows.tsx
@@ -1,25 +1,265 @@
-
-
-import Image from "next/image";
-
-
-import  Spotlight2  from "../ui/spotlight2";
+"use client";
+import Spotlight2 from "../ui/spotlight2";
 import { SectionBadge } from "../ui/section-bade";
+import { useId } from "react";
+import Image, { type ImageProps } from "next/image";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import clsx from "clsx";
 
+import { Container } from "@/components/Container";
+import screenshotContacts from "@/images/screenshots/contacts.png";
+import screenshotInventory from "@/images/screenshots/inventory.png";
+import screenshotProfitLoss from "@/images/screenshots/profit-loss.png";
 
+interface Feature {
+  name: React.ReactNode;
+  summary: string;
+  description: string;
+  image: ImageProps["src"];
+  icon: React.ComponentType;
+}
 
+const features: Array<Feature> = [
+  {
+    name: "Reporting",
+    summary: "Stay on top of things with always up-to-date reporting features.",
+    description:
+      "We talked about reporting in the section above but we needed three items here, so mentioning it one more time for posterity.",
+    image: screenshotProfitLoss,
+    icon: function ReportingIcon() {
+      let id = useId();
+      return (
+        <>
+          <defs>
+            <linearGradient
+              id={id}
+              x1="11.5"
+              y1={18}
+              x2={36}
+              y2="15.5"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop offset=".194" stopColor="#fff" />
+              <stop offset={1} stopColor="#6692F1" />
+            </linearGradient>
+          </defs>
+          <path
+            d="m30 15-4 5-4-11-4 18-4-11-4 7-4-5"
+            stroke={`url(#${id})`}
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </>
+      );
+    },
+  },
+  {
+    name: "Inventory",
+    summary:
+      "Never lose track of what’s in stock with accurate inventory tracking.",
+    description:
+      "We don’t offer this as part of our software but that statement is inarguably true. Accurate inventory tracking would help you for sure.",
+    image: screenshotInventory,
+    icon: function InventoryIcon() {
+      return (
+        <>
+          <path
+            opacity=".5"
+            d="M8 17a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z"
+            fill="#fff"
+          />
+          <path
+            opacity=".3"
+            d="M8 24a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z"
+            fill="#fff"
+          />
+          <path
+            d="M8 10a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z"
+            fill="#fff"
+          />
+        </>
+      );
+    },
+  },
+  {
+    name: "Contacts",
+    summary:
+      "Organize all of your contacts, service providers, and invoices in one place.",
+    description:
+      "This also isn’t actually a feature, it’s just some friendly advice. We definitely recommend that you do this, you’ll feel really organized and professional.",
+    image: screenshotContacts,
+    icon: function ContactsIcon() {
+      return (
+        <>
+          <path
+            opacity=".5"
+            d="M25.778 25.778c.39.39 1.027.393 1.384-.028A11.952 11.952 0 0 0 30 18c0-6.627-5.373-12-12-12S6 11.373 6 18c0 2.954 1.067 5.659 2.838 7.75.357.421.993.419 1.384.028.39-.39.386-1.02.036-1.448A9.959 9.959 0 0 1 8 18c0-5.523 4.477-10 10-10s10 4.477 10 10a9.959 9.959 0 0 1-2.258 6.33c-.35.427-.354 1.058.036 1.448Z"
+            fill="#fff"
+          />
+          <path
+            d="M12 28.395V28a6 6 0 0 1 12 0v.395A11.945 11.945 0 0 1 18 30c-2.186 0-4.235-.584-6-1.605ZM21 16.5c0-1.933-.5-3.5-3-3.5s-3 1.567-3 3.5 1.343 3.5 3 3.5 3-1.567 3-3.5Z"
+            fill="#fff"
+          />
+        </>
+      );
+    },
+  },
+];
+
+function Feature({
+  feature,
+  isActive,
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div"> & {
+  feature: Feature;
+  isActive: boolean;
+}) {
+  return (
+    <div
+      className={clsx(className, !isActive && "opacity-75 hover:opacity-100")}
+      {...props}
+    >
+      <div
+        className={clsx(
+          "w-9 rounded-lg",
+          isActive ? "bg-blue-600" : "bg-slate-500"
+        )}
+      >
+        <svg aria-hidden="true" className="h-9 w-9" fill="none">
+          <feature.icon />
+        </svg>
+      </div>
+      <h3
+        className={clsx(
+          "mt-6 text-sm font-medium",
+          isActive ? "text-blue-600" : "text-slate-600"
+        )}
+      >
+        {feature.name}
+      </h3>
+      <p className="mt-2 font-display text-xl text-slate-900">
+        {feature.summary}
+      </p>
+      <p className="mt-4 text-sm text-slate-600">{feature.description}</p>
+    </div>
+  );
+}
+
+function FeaturesMobile() {
+  return (
+    <div className="-mx-4 mt-20 flex flex-col gap-y-10 overflow-hidden px-4 sm:-mx-6 sm:px-6 lg:hidden">
+      {features.map((feature) => (
+        <div key={feature.summary}>
+          <Feature feature={feature} className="mx-auto max-w-2xl" isActive />
+          <div className="relative mt-10 pb-10">
+            <div className="absolute -inset-x-4 bottom-0 top-8 bg-slate-200 sm:-inset-x-6" />
+            <div className="relative mx-auto w-[52.75rem] overflow-hidden rounded-xl bg-white shadow-lg shadow-slate-900/5 ring-1 ring-slate-500/10">
+              <Image
+                className="w-full"
+                src={feature.image}
+                alt=""
+                sizes="52.75rem"
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function FeaturesDesktop() {
+  return (
+    <TabGroup className="hidden lg:mt-20 lg:block">
+      {({ selectedIndex }) => (
+        <>
+          <TabList className="grid grid-cols-3 gap-x-8">
+            {features.map((feature, featureIndex) => (
+              <Feature
+                key={feature.summary}
+                feature={{
+                  ...feature,
+                  name: (
+                    <Tab className="ui-not-focus-visible:outline-none">
+                      <span className="absolute inset-0" />
+                      {feature.name}
+                    </Tab>
+                  ),
+                }}
+                isActive={featureIndex === selectedIndex}
+                className="relative"
+              />
+            ))}
+          </TabList>
+          <TabPanels className="relative mt-20 overflow-hidden rounded-3xl bg-slate-200 py-16 xl:px-16">
+            <div className="-mx-6 flex">
+              {features.map((feature, featureIndex) => (
+                <TabPanel
+                  static
+                  key={feature.summary}
+                  className={clsx(
+                    "px-5 transition duration-500 ease-in-out ui-not-focus-visible:outline-none",
+                    featureIndex !== selectedIndex && "opacity-60"
+                  )}
+                  style={{ transform: `translateX(-${selectedIndex * 100}%)` }}
+                  aria-hidden={featureIndex !== selectedIndex}
+                >
+                  <div className="w-[52.75rem] overflow-hidden rounded-xl bg-white shadow-lg shadow-slate-900/5 ring-1 ring-slate-500/10">
+                    <Image
+                      className="w-full"
+                      src={feature.image}
+                      alt=""
+                      sizes="52.75rem"
+                    />
+                  </div>
+                </TabPanel>
+              ))}
+            </div>
+            <div className="pointer-events-none absolute inset-0 rounded-3xl ring-1 ring-inset ring-slate-900/10" />
+          </TabPanels>
+        </>
+      )}
+    </TabGroup>
+  );
+}
+
+export function SecondaryFeatures() {
+  return (
+    <section
+      id="secondary-features"
+      aria-label="Features for simplifying everyday business tasks"
+      className="pb-14 pt-20 sm:pb-20 sm:pt-32 lg:pb-32"
+    >
+      <Container>
+        <div className="mx-auto max-w-2xl md:text-center">
+          <h2 className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl">
+            Simplify everyday business tasks.
+          </h2>
+          <p className="mt-4 text-lg tracking-tight text-slate-700">
+            Because you’d probably be a little confused if we suggested you
+            complicate your everyday business tasks instead.
+          </p>
+        </div>
+        <FeaturesMobile />
+        <FeaturesDesktop />
+      </Container>
+    </section>
+  );
+}
 
 export default function Workflows() {
   return (
     <section>
-      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6">
         <div className="pb-12 md:pb-20">
           {/* Section header */}
-          <div className="mx-auto max-w-3xl pb-12 text-center md:pb-20">
-            <div className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-gradient-to-r before:from-transparent before:to-blue-200/50 after:h-px after:w-8 after:bg-gradient-to-l after:from-transparent after:to-blue-200/50">
-              <SectionBadge title="Advanced Aviation Solutions" />
-            </div>
-            <div className="mx-auto max-w-3xl pb-12 text-center md:pb-20">
+          {/* <div className="mx-auto max-w-3xl pb-12 text-center md:pb-20"> */}
+          <div className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-gradient-to-r before:from-transparent before:to-blue-200/50 after:h-px after:w-8 after:bg-gradient-to-l after:from-transparent after:to-blue-200/50">
+            <SectionBadge title="Advanced Aviation Solutions" />
+          </div>
+          {/* <div className="mx-auto max-w-3xl pb-12 text-center md:pb-20">
               <h2 className="pt-10 animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.700),theme(colors.gray.700),theme(colors.gray.700),theme(colors.gray.700),theme(colors.gray.500))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle text-3xl font-semibold text-transparent md:text-5xl">
                 How Our Learning Analytics <br />
                 Enhances Aviation Training
@@ -32,17 +272,17 @@ export default function Workflows() {
                 individual needs.
               </p>
             </div>
-          </div>
+          </div> */}
           {/* Spotlight items */}
-          <Spotlight2 className="group mx-auto grid max-w-sm items-start gap-6 lg:max-w-none lg:grid-cols-3">
-            {/* Card 1 */}
-            <a
+
+          <SecondaryFeatures />
+          {/* <Spotlight2 className="group mx-auto grid max-w-sm items-start gap-6 lg:max-w-none lg:grid-cols-3">
+             <a
               className="group/card relative h-full overflow-hidden rounded-2xl border-blue-700 bg-gray-200 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-gray-400/20 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-gray-500/80 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
               href="#0"
             >
               <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-100 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-300/50 after:via-gray-300/25 after:to-gray-300/50">
-                {/* Arrow */}
-                <div
+                 <div
                   className="absolute right-6 top-6 flex h-8 w-8 items-center justify-center rounded-full border border-gray-700/50 bg-gray-800/65 text-gray-200 opacity-0 transition-opacity group-hover/card:opacity-100"
                   aria-hidden="true"
                 >
@@ -58,16 +298,14 @@ export default function Workflows() {
                     />
                   </svg>
                 </div>
-                {/* Image */}
-                <Image
+                 <Image
                   className="inline-flex"
                   src="/images/workflow-01.png"
                   width={350}
                   height={288}
                   alt="Workflow 01"
                 />
-                {/* Content */}
-                <div className="p-6">
+                 <div className="p-6">
                   <div className="mb-3">
                     <span className="btn-sm relative rounded-full bg-gray-200/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
                       <span className="bg-gradient-to-r from-blue-700 to-blue-700 bg-clip-text text-transparent">
@@ -84,13 +322,11 @@ export default function Workflows() {
                 </div>
               </div>
             </a>
-            {/* Card 2 */}
-            <a
+             <a
               className="group/card relative h-full overflow-hidden rounded-2xl border-blue-700 bg-gray-200 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-gray-400/20 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-gray-500/80 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
               href="#0"
             >
               <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-100 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-300/50 after:via-gray-300/25 after:to-gray-300/50">
-                {/* Arrow */}
                 <div
                   className="absolute right-6 top-6 flex h-8 w-8 items-center justify-center rounded-full border border-gray-700/50 bg-gray-800/65 text-gray-200 opacity-0 transition-opacity group-hover/card:opacity-100"
                   aria-hidden="true"
@@ -107,7 +343,6 @@ export default function Workflows() {
                     />
                   </svg>
                 </div>
-                {/* Image */}
                 <Image
                   className="inline-flex"
                   src="/images/workflow-02.png"
@@ -115,7 +350,6 @@ export default function Workflows() {
                   height={288}
                   alt="Workflow 02"
                 />
-                {/* Content */}
                 <div className="p-6">
                   <div className="mb-3">
                     <span className="btn-sm relative rounded-full bg-gray-200/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
@@ -132,13 +366,11 @@ export default function Workflows() {
                 </div>
               </div>
             </a>
-            {/* Card 3 */}
             <a
               className="group/card relative h-full overflow-hidden rounded-2xl border-blue-700 bg-gray-200 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-gray-400/20 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-gray-500/80 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
               href="#0"
             >
               <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-100 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-300/50 after:via-gray-300/25 after:to-gray-300/50">
-                {/* Arrow */}
                 <div
                   className="absolute right-6 top-6 flex h-8 w-8 items-center justify-center rounded-full border border-gray-700/50 bg-gray-800/65 text-gray-200 opacity-0 transition-opacity group-hover/card:opacity-100"
                   aria-hidden="true"
@@ -155,7 +387,6 @@ export default function Workflows() {
                     />
                   </svg>
                 </div>
-                {/* Image */}
                 <Image
                   className="inline-flex"
                   src="/images/workflow-03.png"
@@ -163,7 +394,6 @@ export default function Workflows() {
                   height={288}
                   alt="Workflow 03"
                 />
-                {/* Content */}
                 <div className="p-6">
                   <div className="mb-3">
                     <span className="btn-sm relative rounded-full bg-gray-200/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
@@ -180,7 +410,7 @@ export default function Workflows() {
                 </div>
               </div>
             </a>
-          </Spotlight2>
+          </Spotlight2> */}
         </div>
       </div>
     </section>
