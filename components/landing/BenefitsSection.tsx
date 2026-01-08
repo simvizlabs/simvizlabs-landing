6bad4b768a8d3ecddbb6b7076258f1e050f54f36
pranjalkatiyar <pranjalkatiyar21@gmail.com>
2026-01-08 15:42:46 +0530

    benefits section

--- components/landing/BenefitsSection.tsx
+++ components/landing/BenefitsSection.tsx
@@ -19,13 +19,13 @@ const benefits = [
 export function BenefitsSection() {
   return (
     <section
-      className="relative w-full pb-20 md:pb-[25rem] lg:pb-[35rem] mx-auto py-12 md:py-24 overflow-hidden"
+      className="relative w-full pt-20 sm: pb-[15rem] md:pb-[15rem] lg:pb-[35rem] sm:mx-auto  md:py-24 overflow-hidden"
       style={{
         background: 'radial-gradient(170.74% 170.74% at 50% -101.23%, #FFF 36.45%, #C1E1FF 42%, #F5F5F7 100%)'
       }}
     >
       {/* Background Image */}
-      <div className="absolute right-[-2rem] sm:right-[-5rem] md:right-[-10rem] lg:right-[-15rem] md:top-0 scale-[1.2] md:scale-[2] lg:scale-[3] w-full md:w-1/2 h-full z-0 opacity-20 sm:opacity-50 md:opacity-100 pointer-events-none">
+      <div className="absolute hidden sm:block text-center text-bottom sm:top-[10rem] md:top-[8rem] right-[calc(100%-45rem)] sm:right-[-22rem] md:right-[-10rem] lg:right-[-15rem] md:top-0 scale-[1.2] md:scale-[2] lg:scale-[3] w-full md:w-1/2 h-full z-0 md:opacity-100 pointer-events-none">
         <Image
           src="/landing/bg-benefits.png"
           alt="Benefits Background"
@@ -34,7 +34,7 @@ export function BenefitsSection() {
         />
       </div>
 
-      <div className="container relative z-10 mx-auto px-4 sm:px-8">
+      <div className="relative z-10 mx-auto px-4 sm:px-8">
         <h2 className="mb-10 sm:mb-16 max-w-2xl text-3xl sm:text-4xl font-bold leading-tight tracking-tight text-black sm:text-5xl">
           Train Smarter with Real Airline Systems.
         </h2>
